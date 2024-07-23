fc02f60b8469075abd696fa315dd2fd5b3d5dc52
Tapish Jain <tapishj@splunk.com>
2024-07-23 11:58:55 -0700

    PAPP-34390: Get user action working

--- gsgmail_connector.py
+++ gsgmail_connector.py
@@ -72,6 +72,8 @@ class GSuiteConnector(BaseConnector):
         super(GSuiteConnector, self).__init__()
 
     def _create_service(self, action_result, scopes, api_name, api_version, delegated_user=None):
+        self.debug_print("key json:")
+        self.debug_print(self._key_dict)
 
         # first the credentials
         try:
@@ -673,7 +675,7 @@ class GSuiteConnector(BaseConnector):
             summary['next_page_token'] = next_page
 
         return action_result.set_status(phantom.APP_SUCCESS)
-    
+
     def _handle_get_user(self, param):
 
         self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
@@ -686,21 +688,21 @@ class GSuiteConnector(BaseConnector):
 
         self.save_progress("Creating AdminSDK service object")
 
-        ret_val, service = self._create_service(action_result, scopes, "admin", "directory_v1", param["email"])
+        ret_val, service = self._create_service(action_result, scopes, "gmail", "v1", param["email"])
 
         if phantom.is_fail(ret_val):
             return action_result.get_status()
-        
+
         try:
-            user_info = service.users().getProfile().execute()
+            user_info = service.users().getProfile(userId="me").execute()
         except Exception as e:
             error_message = self._get_error_message_from_exception(e)
             return action_result.set_status(phantom.APP_ERROR, GSGMAIL_USER_FATCH_FAILED, error_message)
-        
+
         action_result.add_data(user_info)
-        
+
         return action_result.set_status(phantom.APP_SUCCESS)
-    
+
     def _handle_test_connectivity(self, param):
 
         self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
